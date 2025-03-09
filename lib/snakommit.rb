e49f9988b978fd54e111cf2e2a4b162ee0354b02
Antonia <antonia.dev@icloud.com>
2025-03-09 14:13:36 +0100

    feat: add main application module

---
+++ lib/snakommit.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'snakommit/version'
+require 'snakommit/config'
+require 'snakommit/git'
+require 'snakommit/prompt'
+require 'snakommit/cli'
+
+# Main module for the Snakommit application
+module Snakommit
+  class Error < StandardError; end
+  # Your code goes here...
+end 
